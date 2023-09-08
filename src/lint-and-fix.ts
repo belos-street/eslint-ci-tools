87ef352ccd8f13ef47342c8b04aeab0e55dc7365
zhi.jiang <zhi.jiang@sectrend.com.cn>
2023-09-08 11:29:59 +0800

    feat: add message count

--- src/lint-and-fix.ts
+++ src/lint-and-fix.ts
@@ -3,5 +3,12 @@ import { ESLint } from 'eslint'
 export async function lintAndFix(eslint: ESLint, filePaths: string) {
   const results = await eslint.lintFiles(filePaths)
   // await ESLint.outputFixes(results)   //fix code
+
+  countInfo(results)
   return results
+}
+
+function countInfo(results: ESLint.LintResult[]) {
+  const count: number = results.reduce((pre, item) => (pre += item.messages.length), 0)
+  console.log(`messages number is: ${count}`)
 }
